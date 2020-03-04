9acf8f43a37694b8e0998f45838c651c0ebaea01
angel <angel@DESKTOP-27E4LQM>
2020-03-03 23:26:33 -0300

    Tratando multiplas excessoes

---
+++ src/main/java/br/com/home/Fluxo.java
@@ -0,0 +1,31 @@
+package br.com.home;
+
+public class Fluxo {
+	public static void main(String[] args) {
+		System.out.println("Inicio do main");
+		try {
+			metodo1();
+		} catch (ArithmeticException | NullPointerException | MinhaExcecao ex) {
+			String msg = ex.getMessage();
+			System.out.println("Exception" + msg);
+			ex.printStackTrace();
+		}
+		System.out.println("Fim do main");
+	}
+
+	public static void metodo1() throws MinhaExcecao {
+		System.out.println("Inicio do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+
+	}
+
+	public static void metodo2() throws MinhaExcecao {
+		System.out.println("Inicio do metodo2");
+		boolean lancaExcecao = true; // Para compilar o ultimo comando
+		if (lancaExcecao) {
+			throw new MinhaExcecao("Lançando minha excessao");
+		}
+		System.out.println("Fim do metodo2");
+	}
+}
---
+++ src/main/java/br/com/home/MinhaExcecao.java
@@ -0,0 +1,9 @@
+package br.com.home;
+
+public class MinhaExcecao extends Exception {
+
+	public MinhaExcecao(String msg) {
+		super(msg);
+	}
+
+}
