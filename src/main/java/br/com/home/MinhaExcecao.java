a2af50d657a2f64ab86153270178b4ceb77efa9c
angel <angel@DESKTOP-27E4LQM>
2020-06-13 09:08:05 -0300

    Cap04-Video04 - implementação completa

--- src/main/java/br/com/home/MinhaExcecao.java
+++ src/main/java/br/com/home/MinhaExcecao.java
@@ -1,9 +1,10 @@
 package br.com.home;
 
-// Se esta classe estender Exception, as classes que lançarem esta exceção terão que
+// Se esta classe estender Exception, os métodos que lançarem esta exceção terão que
 //informar o lançamento de exceção na assinatura do método("throws", lança). A Classe se
 //torna uma exceção "checked".
 
+// Classes do tipo RuntimeException são "uncheced"
 public class MinhaExcecao extends RuntimeException {
 
 	public MinhaExcecao(String msg) {
