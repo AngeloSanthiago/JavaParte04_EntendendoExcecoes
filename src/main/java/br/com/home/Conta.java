5ae55ed97261e1c4faa47a5119a5326202b54d34
angel <angel@DESKTOP-27E4LQM>
2020-06-14 09:18:03 -0300

    Cap05-Video04 - classe conta com retorno booleano

--- src/main/java/br/com/home/Conta.java
+++ src/main/java/br/com/home/Conta.java
@@ -1,8 +1,26 @@
 package br.com.home;
 
-public class Conta {
+public abstract class Conta {
+	private double saldo;
 	void deposita() throws MinhaExcecao {
 		System.out.println("inicio deposito");
 		throw new MinhaExcecao("Falha no deposito");
 	}
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saca(valor)) {
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public abstract void deposita(double valor);
 }
